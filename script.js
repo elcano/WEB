35caa6692c85a45f04a964e2994661e09fe4e95d
AbbyHoyt <abby.hoyt@comcast.net>
2026-08-24 00:12:30 -0700

    Started implementing navbar menu for mobile.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+/*Adjust Navbar Depending on Screen Size*/
+function adjustNavBar() {
+    var x = document.getElementById("globalNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
